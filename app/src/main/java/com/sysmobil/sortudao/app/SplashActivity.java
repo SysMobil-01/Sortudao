3b936cf95ae1c0bd09fc5423a8f8ef0a9bd2e193
Peter <leaop54@gmail.com>
2015-12-15 18:18:46 -0200

    New icon and splash screen

---
+++ app/src/main/java/com/sysmobil/sortudao/app/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.sysmobil.sortudao.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by peter on 15/12/15.
+ */
+public class SplashActivity extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_main);
+
+        Handler hd = new Handler();
+        hd.postDelayed(this, 2000);
+    }
+
+
+    @Override
+    public void run() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
